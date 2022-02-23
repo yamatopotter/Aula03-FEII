4233adc6de5640f427a6544c9fb8aec77cc01360
Lucas Adrian <lucasadrianr@gmail.com>
2022-02-23 20:32:58 -0300

    Adiciona O linha 3 coluna 3

--- mesa-de-trabalho/assets/js/main.js
+++ mesa-de-trabalho/assets/js/main.js
@@ -1,2 +1,6 @@
 var adicionandoX = document.getElementById("linha-2_coluna-2");
 console.log(adicionandoX);
+
+var adicionandoO = document.getElementById("linha-3_coluna-3");
+
+adicionandoO.innerText="O";
