1076018adc3fabc3160db0d35abe5f2b28c26fba
johnnyrps <johnnyrps@gmail.com>
2022-02-23 20:34:41 -0300

    johnnyX

--- mesa-de-trabalho/assets/js/main.js
+++ mesa-de-trabalho/assets/js/main.js
@@ -4,3 +4,7 @@ console.log(adicionandoX);
 var adicionandoO = document.getElementById("linha-3_coluna-3");
 
 adicionandoO.innerText="O";
+
+var adicionandoO = document.getElementById("linha-3_coluna-2");
+
+adicionandoO.innerText="X";
